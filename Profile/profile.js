8b3bdbe3a14774d4e658eeda561d2491b879a5ef
Caleb <S10267482@connect.np.edu.sg>
2025-07-11 08:10:08 +0800

    m

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,5 +1,9 @@
 const name = document.getElementById('name')
 const email = document.getElementById('email')
+const bday = document.getElementById('birth-date')
 
 let profile = []
 
+function displayInfo(){
+    
+}
