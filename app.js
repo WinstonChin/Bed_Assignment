f3e50504eeb1ef7a4305b2d666620cb962755372
WinstonChin <s10266664@connect.np.edu.sg>
2025-07-11 23:42:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.put("/api/meds/:id", validateDateID, validateDate, medsController.updateDate
 app.delete("/api/meds/:id", validateDateID, medsController.deleteDate);
 
 //Auth Meds//
-const { authenticateToken } = require('./Login/authenticate'); // 
+const authenticateToken = require('./Login/authenticate');
+
 
 app.get("/api/meds", authenticateToken, medsController.getAllDates);
 app.get("/api/meds/:id", authenticateToken, validateDateID, medsController.getDateById);
