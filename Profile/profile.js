b54f4a89524d72bfb874bc32c24baa96614092dd
Caleb <S10267482@connect.np.edu.sg>
2025-07-11 08:29:11 +0800

    m

--- Profile/profile.js
+++ Profile/profile.js
@@ -5,5 +5,14 @@ const bday = document.getElementById('birth-date')
 let profile = []
 
 function displayInfo(){
-    
+
 }
+//profile picture//
+  function preview_image(event) {
+    const reader = new FileReader(); //create a reader
+    reader.onload = function(){ // set a handler what to do when done loading
+      const output = document.getElementById('output_image'); // get the node
+      output.src = reader.result; // set the result of the reader as the src of the node
+    }
+    reader.readAsDataURL(event.target.files[0]); // now start the reader
+  }
